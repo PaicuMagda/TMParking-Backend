c6abc0ae2a4f64192c19ef0fe67fc346f6b49c12
Magda Paicu <paicumagda962@gmail.com>
2023-10-21 18:04:40 +0300

    create project and bundle with SqlServer+ create user Model

---
+++ TMParking-Backend/Data/DbContextTMParking.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMParking_Backend.Models;
+
+namespace TMParking_Backend.Data
+{
+    public class DbContextTMParking : DbContext
+    {
+        public DbContextTMParking(DbContextOptions<DbContextTMParking> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+      
+    }
+}
